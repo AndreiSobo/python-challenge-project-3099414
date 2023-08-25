8f7955d9bc111dca34420af206284dab1bf2e083
Andrei Sobo <a.sobo1@uni.brighton.ac.uk>
2023-08-25 14:17:15 +0100

    Implemented different classes using inheritance

--- exercise_files/07_04_challenge.py
+++ exercise_files/07_04_challenge.py
@@ -124,9 +124,135 @@ def circleBottom(x):
 
 canvas = Canvas(30, 30)
 scribe = TerminalScribe(canvas)
-scribe.drawSquare(20)
-#scribe.plotX(sine)
-#scribe.plotX(cosine)
-#scribe.plotX(circleTop)
-#scribe.plotX(circleBottom)
+# scribe.drawSquare(5)
+# scribe.plotX(sine)
+# scribe.plotX(cosine)
+# scribe.plotX(circleTop)
+# scribe.plotX(circleBottom)
+# print(colored('Hello, World!', 'red', 'on_black', ['bold', 'blink']))
+# scribe.plotX(circleTop)
+# scribe.plotX(circleBottom)
+
+# TODO extend TerminalScribe to add new behaviour
+# the classes should use inheritance and 
+# have a graphic scribe, a vector scribe, a scribe that does just up, down, left, right. Can make a custom one
+# create an interface that does fun things with the termcolor module installed earlier
+
+# the classes should allow users to set parameters such as: position, direction and framerate. 
+# these should be set when instantiating the object, and also be able to modify them afterwards(maybe)
+class BlueSquareScribe(TerminalScribe):
+    def __init__(self, framerate, pos):
+        super().__init__(canvas)
+        self.framerate = framerate
+        self.pos = pos
+        self.trail = '.'
+        self.mark = '*'
+
+    def draw(self, pos):
+        self.canvas.setPos(self.pos, self.trail)
+        self.pos = pos
+        self.canvas.setPos(self.pos, colored(self.mark, 'blue'))
+        #print(self.pos)
+        self.canvas.print()
+        time.sleep(self.framerate)
+    
+    def drawSquare(self, size):
+        for i in range(size-1):
+            pos = [self.pos[0]+1, self.pos[1]]
+            self.draw(pos)
+        for i in range(size-1):
+            pos = [self.pos[0], self.pos[1]+1]
+            self.draw(pos)
+        for i in range(size-1):
+            pos = [self.pos[0]-1, self.pos[1]]
+            self.draw(pos)
+        for i in range(size-1):
+            pos = [self.pos[0], self.pos[1]-1]
+            self.draw(pos)
+
+b1 = BlueSquareScribe(0.5, [5,5])
+b2 = BlueSquareScribe(0.2, [10,2])
+# b1.drawSquare(4)
+# b2.drawSquare(5)
+
+class Words(TerminalScribe):
+    def __init__(self, framerate, colour, pos):
+        super().__init__(canvas)
+        self.framerate = framerate
+        self.pos = pos
+        self.colour = colour
+        self.trail = '.'
+        self.mark = colored('*', self.colour)
+    
+    def draw(self, pos):
+        self.canvas.setPos(self.pos, self.trail)
+        self.pos = pos
+        self.canvas.setPos(self.pos, self.mark)
+        self.canvas.print()
+        time.sleep(self.framerate)
+
+    def up(self):
+        pos = [self.pos[0], self.pos[1]-1]
+        self.draw(pos)
+
+    def down(self):
+        pos = [self.pos[0], self.pos[1]+1]
+        self.draw(pos)
+
+    def left(self):
+        pos = [self.pos[0]-1, self.pos[1]]
+        self.draw(pos)
+
+    def right(self):
+        pos = [self.pos[0]+1, self.pos[1]]
+        self.draw(pos)
+
+class DirectionScribe(TerminalScribe):
+    def __init__(self, framerate, trail ='.', mark = '*' ):
+        super().__init__(canvas)
+        self.framerate = framerate
+        self.pos = [0, 0]
+        self.mark = mark
+        self.trail = trail
+        self.direction = [0, 1]
+    def bounce(self, pos):
+        return super().bounce(pos)
+    
+    def forward(self, distance):
+        return super().forward(distance)
+        
+    def setDirection(self, degrees):
+        return super().setDegrees(degrees)
+
+    def setPosition(self, pos):
+        return super().setPosition(pos)
+    
+    def draw(self, pos):
+        return super().draw(pos)
+
+d1 = DirectionScribe(0.8, trail='^', mark="@")
+d1.setDirection(300)
+d1.setPosition([5,5])
+d1.forward(30)
+    
+
+w1 = Words(0.1, "green", [8,1])
+w2 = Words(0.1, "green", [8,4])
+w3 = Words(0.1, "green", [12,1])
+w4 = Words(0.1, "green", [13,1])
+
+def first_let(w1, w2, w3):
+    for _ in range(8):
+        w1.down()
+    for _ in range(4):
+        w2.right()
+    for _ in range(8):
+        w3.down()
+    
+def sec_let(w4):
+    for _ in range(8):
+        w4.down()
+
+# first_let(w1,w2,w3)
+# sec_let(w4)
 
